25ad55866caa3fb42124582f4f2a41295a86a8b0
himanshu79854 <himanshu79854@gmail.com>
2026-05-03 15:10:45 +0530

    add production api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "team-task-manager-production-d07f.up.railway.app/api"
 });
 
 // attach token automatically
